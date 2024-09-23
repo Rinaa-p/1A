9a7a304fe55d3aa7d3b2e26e5860fbb03a417b6f
Rina <pukair@delta-studenti.cz>
2024-09-23 17:10:51 +0300

    nn

--- DatoveTypy/obj/Debug/net8.0/DatoveTypy.AssemblyInfo.cs
+++ DatoveTypy/obj/Debug/net8.0/DatoveTypy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DatoveTypy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+feb5bd9d3c68dd64f2ad853f89ceb36e8de0c2cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d169045b52f9d046ab981450fe3592d4a73cfa87")]
 [assembly: System.Reflection.AssemblyProductAttribute("DatoveTypy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DatoveTypy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ ObsahCtvrce/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class MainClass {
+    public static void Main (string[] args) {
+        // Sem přijde váš kód
+        Console.Write("Napis stranu ctverce: ");
+        int stranaA = Int32.Parse(Console.ReadLine());
+        
+        Console.WriteLine($"Obsah teto ctverce: {stranaA * stranaA}");
+    
+    
+        // Konec prostoru pro kód
+    }
+}
