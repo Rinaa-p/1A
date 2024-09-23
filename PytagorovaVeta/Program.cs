feb5bd9d3c68dd64f2ad853f89ceb36e8de0c2cf
Rina <pukair@delta-studenti.cz>
2024-09-23 15:51:12 +0300

    New project added (PytagorovaVeta)

---
+++ PytagorovaVeta/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class MainClass {
+    public static void Main (string[] args) {
+        // Sem přijde váš kód
+    
+         Console.Write("Prvni odvesna: ");
+         int x = int.Parse(Console.ReadLine());
+         
+         Console.Write("Prepona: ");
+         int y = int.Parse(Console.ReadLine());
+
+         if (y - x >= 1)
+         {
+             double xy = y * y - x * x;
+             double result = Math.Sqrt(xy);
+
+             Console.WriteLine($"Druha odvesna: {result}");
+         }
+         else
+         {
+             Console.WriteLine("Spocitat nelze");
+         }
+         // Konec prostoru pro kód
+    }
+}
