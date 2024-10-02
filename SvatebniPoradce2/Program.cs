7bbd116bd672db3d273051ec2666e8216580636f
Rina <pukair@delta-studenti.cz>
2024-10-02 11:02:36 +0300

    projects added

---
+++ SvatebniPoradce2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class MainClass {
+    public static void Main (string[] args) {
+    
+        // Nastavte si proměnné
+        bool jeMlady = false;
+        bool jeHezky = false;
+        bool jeBohaty = true;
+    
+        // tuto podmínku je třeba doplnit
+        if (jeMlady)
+            // zde končí úpravy
+        {
+            Console.WriteLine("Toho si neber!");
+        }
+        else
+        {
+            Console.WriteLine("Vezmi si ho!");
+        }
+    
+
+    }
+}
