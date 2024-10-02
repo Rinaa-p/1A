ee2ec5af85f6a5416f70e50e6e5b32211da75ec7
Rina <pukair@delta-studenti.cz>
2024-10-02 11:21:22 +0300

    projects added

---
+++ StudijniPoradce/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class MainClass {
+    public static void Main (string[] args) {
+        // Sem přijde váš kód
+        Console.WriteLine("Kolik procent ziskal zak z matematiky?");
+        int math = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Kolik procent ziskal zak z ceskeho jazyka?");
+        int cj = Convert.ToInt32(Console.ReadLine());
+
+        if (math > 66 && cj > 66)
+        {if(math>cj){
+                Console.WriteLine("Doporucujeme studovat technicky obor");
+            }else{
+                Console.WriteLine("Doporucujeme studovat humanitni obor");
+            }
+        }else if (math <= 66){
+            Console.WriteLine("Doporucujeme studovat humanitni obor");
+        }else if (cj <=66){
+            Console.WriteLine("Doporucujeme studovat technicky obor");
+        }else{
+            Console.WriteLine("Doporucujeme studovat remeslny obor");
+        }
+
+
+    }
+}
