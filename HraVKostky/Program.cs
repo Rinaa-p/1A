8aaaae8ff3a0bfb07df4bde18b92b54740829541
Rina <pukair@delta-studenti.cz>
2024-10-14 11:26:35 +0300

    projects added

---
+++ HraVKostky/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class MainClass {
+    public static void Main (string[] args) {
+        // Sem přijde váš kód
+        Random cislo = new Random();
+        int cis = cislo.Next(1, 7);
+        Console.WriteLine($"{cis} je moje cislo, stiskni klavesu at hodi tvoje cislo");
+        Console.ReadKey();
+        Random num = new Random();
+        int number = num.Next(1, 7);
+        if (number > cis)
+        {
+            Console.WriteLine($"\n {number}:{cis} Vyhral jsi");
+        }else if (number < cis){
+            Console.WriteLine($"\n {number}:{cis} Prohral jsi");
+        }else{
+            Console.WriteLine($"\n {number}:{cis} Remiza");
+        }
+    }
+}
