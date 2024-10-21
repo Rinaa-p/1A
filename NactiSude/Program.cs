e413b54f2890483b46d0e7bc3d700346fe05f8e7
Rina <pukair@delta-studenti.cz>
2024-10-21 11:51:16 +0300

    projects added

---
+++ NactiSude/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class MainClass {
+    public static void Main (string[] args) {
+        Console.WriteLine("Zadej sude cislo: ");
+        int i = int.Parse(Console.ReadLine());
+        int a = i % 2;
+        while (a != 0)
+        {
+            Console.WriteLine("To Neni sude, zkus to znovu: ");
+            i = int.Parse(Console.ReadLine());
+            a = i % 2;
+        }
+
+        Console.WriteLine($"Neblizsi liche vyssi cislo je: {i+1}");
+
+    }
+}
---
+++ SudeVetsiNezDeset/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class MainClass {
+    public static void Main (string[] args) {
+        Console.Write("Napis cislo sude a vetsi nez 10: ");
+        int i = int.Parse(Console.ReadLine());
+        while (i % 2 != 0 || i < 10)
+        {
+            Console.WriteLine("To nevyhovuje podminkam (sude a vetsi ne 10), zkos to znovu: ");
+            i = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine($"zadal jsi {i}");
+
+
+    }
+}
