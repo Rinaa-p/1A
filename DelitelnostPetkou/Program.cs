b798bb7d15808c69b3c3ef931cb82e699d6251ca
Rina <pukair@delta-studenti.cz>
2024-10-21 12:05:51 +0300

    projects added

---
+++ DelitelnostPetkou/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Napis cislo delitelne 5: ");
+        int i = int.Parse(Console.ReadLine());
+
+        while (i % 5 != 0)
+        {
+            Console.Write("To neni delitelne 5, zkus to znovu: ");
+            i = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine($"{i} / 5 = {i/5}");
+    }
+}
---
+++ SoucetZadanychCisel/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Zadej cisla, ja jej budu scitat. Napis 0 kdyz ukoncis.");
+        int i = int.Parse(Console.ReadLine());
+        int a = i;
+        
+
+        while (i != 0)
+        {
+            Console.Write("Napis dalsi cislo: ");
+            i = int.Parse(Console.ReadLine());
+            a += i;
+        }
+
+        Console.WriteLine($"Soucet zadanych cisel: {a}");
+    }
+}
