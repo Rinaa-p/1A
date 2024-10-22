626bd8dcf5d421aca39d7ac5ab36052aeda17bf5
Rina <pukair@delta-studenti.cz>
2024-10-22 12:03:52 +0300

    projects added

---
+++ SoucetCiselVIntervalu/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Zadej jeden kraj intervalu: ");
+        int i = int.Parse(Console.ReadLine());
+        Console.Write("Zadej druhy kraj intervalu: ");
+        int a = int.Parse(Console.ReadLine());
+        int x = i;
+        int y = 0;
+        while (x != a + 1)
+        {
+            y += x;
+            x++;
+        }
+
+        Console.WriteLine(y);
+
+    }
+}
